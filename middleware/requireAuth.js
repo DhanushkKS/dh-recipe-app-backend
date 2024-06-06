70a080591fb3a4b0cb2eafd5154689515dab5c23
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-06-06 20:02:07 +0530

    Feat: Add require Authentication for access recipes

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
-const requireAuth = (req, res, next) => {
+const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({ error: "Authorization token required" });
   }
   const token = authorization.split(" ")[1];
   try {
-    const { _id } = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = User.findOne({ _id: _id }).select("_id");
+    const { _id } = await jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findOne({ _id: _id }).select("_id");
     next();
   } catch (error) {
     res.status(401).json({ error: "Request is not authorized" });
